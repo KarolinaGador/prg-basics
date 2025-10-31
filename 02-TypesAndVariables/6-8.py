0dd6f8a736eef41279b993182cfc442f072373ba
KarolinaGador <kjgador8@gmail.com>
2025-10-31 17:31:21 +0100

    zadania2

---
+++ 02-TypesAndVariables/6-8.py
@@ -0,0 +1,10 @@
+###
+# A program that calculates
+# how many letters are between two given letters
+#
+first = input('Enter first letter: ')
+last = input('enter last letter: ')
+first_letter_code = ord(first)
+last_letter_code = ord(last)
+number_of_letters = int(last_letter_code - first_letter_code )
+print(f'Between {first} and {last} is {number_of_letters} letters')
