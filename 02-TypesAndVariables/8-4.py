f53469fd6bc8b867709b89deab8101fd2d73fa36
KarolinaGador <kjgador8@gmail.com>
2025-11-01 11:03:00 +0100

    zad koniec

--- 02-TypesAndVariables/8-4.py
+++ 02-TypesAndVariables/8-4.py
@@ -2,8 +2,8 @@
 # A program that prints your height both in cm and in feet and inches.
 #
 cm = 170
-feet = ...
-inches = ...
+feet = cm//30.48 
+inches = cm/2.54%12
 # calculate the number of feet
 
-print(f'I am {cm}cm tall, i.e. ... feet and ... inches')
+print(f'I am {cm}cm tall, i am {feet} feet and {inches} inches')
---
+++ 02-TypesAndVariables/8-7.py
@@ -0,0 +1,4 @@
+decimal_number = int(input("decimal number: "))
+binary_number = bin(decimal_number)
+hex_number = hex(decimal_number)
+print(f'{decimal_number} is {binary_number} in binary code and {hex_number} in hex code')
