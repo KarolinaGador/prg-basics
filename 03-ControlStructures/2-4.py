6bfb1b15a2bd960002a08aa8339a7eaf25d614f8
KarolinaGador <kjgador8@gmail.com>
2025-11-07 12:51:12 +0100

    ere

--- 03-ControlStructures/2-4.py
+++ 03-ControlStructures/2-4.py
@@ -4,18 +4,19 @@
 # and two numbers. The program should perform the appropriate
 # mathematical operation on the given numbers and return the result.   
 # 
-number1 = input("Enter first number: ")
-number2 = input ("Enter second number: ")
-operator = input("Enter operator(+/-/*//)")
+number1 = int(input("Enter first number: "))
+number2 = int(input ("Enter second number: "))
+operator = int(input("Enter operator(+/-/*//)"))
 
 if operator == "+":
-    result = number1+number2
+    result = number1 + number2
 elif operator == "-":
-result = number1-number2
+    result = number1-number2
+
 elif operator == "*":
-result = number1*number2
+    result = number1*number2
 elif operator == "/":
-result = number1/number2
+    result = number1/number2
 
 
 # print result
