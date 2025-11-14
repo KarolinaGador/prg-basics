642f9d5c3b9ca40b81ee99cd91efd310cf7acac7
KarolinaGador <kjgador8@gmail.com>
2025-11-14 12:55:44 +0100

    1

--- 04-Functions/4-3.py
+++ 04-Functions/4-3.py
@@ -5,9 +5,9 @@
 import math
 def triangle_area(a,b,c):
     s = 1/2 * (a+b+c)
-    result= float(math.sqrt(s(s - a)(s - b)(s - c) ))
+    result= float(math.sqrt(s*(s - a)*(s - b)*(s - c) ))
 
 
     return result
 values = triangle_area(int(input('Enter 1 number: ')), int(input('Enter 2 number: ')), int(input('Enter 3 number: ')))
-print(f'The area of ​​a triangle with sides {} is {values}')
+print(f'The area of a triangle with sides is {values}')
