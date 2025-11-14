6c2036a15b387a4bf18e705ec9a371be5890777f
KarolinaGador <kjgador8@gmail.com>
2025-11-14 12:53:38 +0100

    zadania

--- 03-ControlStructures/4-7.py
+++ 03-ControlStructures/4-7.py
@@ -4,7 +4,7 @@
 sum = 0
 
 for i in range(1,11):
-    if not i%2 != 0 :
+    if i%2 != 0 :
       continue
     sum += i
     
---
+++ 03-ControlStructures/4-8.py
@@ -0,0 +1,8 @@
+###
+# Calculates values for the following fractions: 1/2, 1/3, ..., 1/10
+#
+
+for i in range(1,11):
+  result =int(1/i)
+
+print(f'1/{} = {result}')
