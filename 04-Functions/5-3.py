f5848bc67f165e57eaba2219bbc8001afcda4dd0
KarolinaGador <kjgador8@gmail.com>
2025-11-18 18:02:50 +0100

    ew

--- 04-Functions/5-3.py
+++ 04-Functions/5-3.py
@@ -9,7 +9,7 @@ import keyboardd # your own defined module
 first_name = keyboardd.input_string('Enter name: ')
 last_name = keyboardd.input_string('Enter name: ')
 age = keyboardd.input_integer('enter age: ')
-salary = keyboardd.input_boolean('enter salary:')
+salary = keyboardd.input_integer('enter salary:')
 is_salary_hidden = keyboardd.input_boolean('Hide salary? (y/n)')
 
 # Prints employee's record
@@ -18,5 +18,5 @@ print('===========')
 print(f'Name:', {first_name})
 print(f'Last:', {last_name})
 print(f'Age:', {age})
-if salary==True:
-    print('Salary')
+if is_salary_hidden==False:
+    print(f'{salary}')
