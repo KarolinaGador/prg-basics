951206daa846c26aa28eaaee0429c0a5e25f7c75
KarolinaGador <kjgador8@gmail.com>
2025-12-05 10:42:01 +0100

    zad

---
+++ 08-FileHandling/1-8.py
@@ -0,0 +1,11 @@
+def read_from_file(name):
+   with open(name) as file:
+      content = file.read()
+   return content
+
+# reads the entire file and splits lines into array
+file_content = read_from_file('pets.txt')
+file_lines = file_content.split()
+num = len(file_lines)
+print(num)
+   
